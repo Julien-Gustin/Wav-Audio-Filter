9509f5a3b29f7191355a55ed9e803d4d77e52c54
HobbesK <57399187+HobbesK@users.noreply.github.com>
2020-03-21 13:51:52 +0100

    Add files via upload
    Ce dont on a parlé : méthodes double[] getOutput() et void giveInput(double[] input)

--- src/DelayFilter.java
+++ src/DelayFilter.java
@@ -6,6 +6,7 @@ public class DelayFilter implements Filter
   private int delay;
   private Queue sQueue;
   private int compt;
+  private int needsInput;
 
   public int nbInputs(){return 1;}
   public int nbOutputs(){return 1;}
@@ -21,6 +22,7 @@ public class DelayFilter implements Filter
     this.delay = delay;
     compt = delay;
     sQueue = new Queue();
+    needsInput = 0;
   }
 
   /*
@@ -30,8 +32,14 @@ public class DelayFilter implements Filter
    */
   public double[] computeOneStep(double[] input) throws FilterException
   {
+    if(needsInput==1)
+      throw new FilterException ("No previous input.");
+
     if(delay < 0)
-      throw new FilterException("delay should be strictly positive");
+      throw new FilterException("Delay should be strictly positive. ");
+
+    if (input.length != 1)
+      throw new FilterException ("Wrong input length. ");
 
     double[] output = new double [1];
     sQueue.put(Double.valueOf(input[0]));
@@ -45,6 +53,40 @@ public class DelayFilter implements Filter
       output[0] = (Double)sQueue.get();
 
     return output;
+  }
+
+  public double[] getOutput() throws FilterException
+  {
+    if(needsInput==1)
+      throw new FilterException ("No previous input.");
+
+    if(delay < 0)
+      throw new FilterException("Delay should be strictly positive. ");
+
+    double[] output = new double [1];
+
+    if(compt > 0)
+    {
+      output[0] = 0;
+      compt--;
+    }
+    else
+      output[0] = (Double)sQueue.get();
+
+    needsInput = 1;
+    return output;
+  }
+
+  public void giveInput() throws FilterException
+  {
+    if(needsInput==0)
+      throw new FilterException ("No need of new input.");
+
+    if (input.length != 1)
+      throw new FilterException ("Wrong input length. ");
+
+    sQueue.put(Double.valueOf(input[0]));
+    needsInput = 0;
   }
 
   public void reset(){
